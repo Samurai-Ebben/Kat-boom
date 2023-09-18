36280a6922d908454d3b7708409be9caa98acd69
SebbGameCreator <SEB@live.se>
2023-09-18 14:40:14 +0200

    CollisionChange

--- Assets/Scripts/BoxDestroyed.cs
+++ Assets/Scripts/BoxDestroyed.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    // Start is called before the first frame update
+ 
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject, 1.5f);
+        }
+    }
+
 }
