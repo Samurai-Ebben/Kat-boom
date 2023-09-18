c5a0c85c8b881b308fd91d6f81bee9ded3bdb920
SebbGameCreator <SEB@live.se>
2023-09-18 15:14:41 +0200

    BoxesAdded

--- Assets/Scripts/BoxDestroyed.cs
+++ Assets/Scripts/BoxDestroyed.cs
@@ -4,22 +4,12 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
- 
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
-            Destroy(gameObject, 1.5f);
+            Destroy(gameObject, 1);
         }
     }
 
