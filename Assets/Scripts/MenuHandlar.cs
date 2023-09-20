2c59a912af783a682061672b31ad743d38f98c9b
Ebben <abanalhindi47@gmail.com>
2023-09-21 00:24:25 +0200

    Add clip how to play

--- Assets/Scripts/MenuHandlar.cs
+++ Assets/Scripts/MenuHandlar.cs
@@ -7,17 +7,24 @@ public class MenuHandlar : MonoBehaviour
 {
     public GameObject menuCanvas;
     public GameObject explosion;
+    public GameObject tutClip;
     public Transform strBtnTransform;
     public void StartGame()
     {
-       
-        Instantiate(explosion, strBtnTransform.position, Quaternion.identity);
-        Invoke("LoadScenesOV", 0.2f);
-        if(Time.timeScale == 0)
+        var ex= Instantiate(explosion, strBtnTransform.position, Quaternion.identity);
+        Destroy(ex, .2f);
+
+        Invoke("ActiveClip", 0.2f);
+        if (Time.timeScale == 0)
             Time.timeScale = 1;
     }
 
-    void LoadScenesOV()
+    void ActiveClip()
+    {
+        menuCanvas.SetActive(false);
+        tutClip.SetActive(true);
+    }
+    public void LoadScenesOV()
     {
         SceneManager.LoadScene(1);
 
